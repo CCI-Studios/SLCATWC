78591dbc12575c3e2e9fc3b46c06b70fa0afd26e
Chris Morris <cmorris@ccistudios.com>
2014-03-13 13:25:20 -0400

    Added arrows to the accordion headings

--- sites/all/themes/slcatwc_custom/js/accordion.js
+++ sites/all/themes/slcatwc_custom/js/accordion.js
@@ -1,8 +1,9 @@
 (function($) {
 	$(function() {
 		$(".accordion").find("h3").click(function(){
+			$(".accordion > h3").not($(this)).removeClass("open");
 			$(".accordion > div").not($(this).next()).removeClass("open");
-			$(this).next().toggleClass("open");
+			$(this).toggleClass("open").next().toggleClass("open");
 		});
 	});
 }(jQuery));
